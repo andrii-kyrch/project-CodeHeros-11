9618646b386cc677bd780238939bfda836c48c01
Andrii Kyrch <andrii.kyrch@gmail.com>
2024-10-15 15:56:27 +0200

    Reviews approve

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -2,40 +2,38 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api';
 
-const reviewsList = document.querySelector(".main-list");
+const reviewsList = document.querySelector('.main-list');
 
-window.addEventListener("load", (event) => {
-    
-    loadReviews();
-})  
+window.addEventListener('load', event => {
+  loadReviews();
+});
 
 function renderReviews(reviews) {
-
-    const htmlMarkup = reviews
-    .map((review) => 
+  const htmlMarkup = reviews
+    .map(
+      review =>
         `<li class="review-item">
             <div class="review-style">
-                <p class="review-text">${review["review"]}</p>
+                <p class="review-text">${review['review']}</p>
                 <div class="user-card">
-                <img class="avatar" src="${review["avatar_url"]}" alt="" />
-                <p class="userName">${review["author"]}</p>  
+                <img class="avatar" src="${review['avatar_url']}" alt="" />
+                <p class="userName">${review['author']}</p>  
                 </div> 
             </div>
-        </li>`)
-    .join("");
+        </li>`
+    )
+    .join('');
 
-    reviewsList.innerHTML = htmlMarkup;
-}      
+  reviewsList.innerHTML = htmlMarkup;
+}
 
 async function loadReviews() {
+  const response = await getReviews();
+  const reviews = response['data'];
 
-    const response = await getReviews();
-    const reviews = response["data"];
-
-    renderReviews(reviews);
+  renderReviews(reviews);
 }
 
 async function getReviews() {
-
-    return await axios.get('/reviews');
+  return await axios.get('/reviews');
 }
