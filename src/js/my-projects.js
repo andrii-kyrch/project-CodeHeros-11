73541b99d326b5bfc40e58effd95ef6e256f4592
Andrii Kyrch <andrii.kyrch@gmail.com>
2024-10-18 10:33:53 +0200

    fix my project js and load more button

--- src/js/my-projects.js
+++ src/js/my-projects.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
@@ -7,97 +6,6 @@ const projectList = document.querySelector('.my-projects-list');
 let projectsLoaded = 0;
 const projectsPerLoad = 3;
 
-
-async function fetchProjects() {
-  try {
-    const response = await axios.get('/api/projects'); 
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching projects:', error);
-    iziToast.error({
-      title: 'Error',
-      message: 'Failed to load projects. Please try again later.',
-      position: 'topRight',
-    });
-    return [];
-  }
-}
-
-// Список всіх проєктів 
-const totalProjects = [
-  {
-    imgSrc: '/img/img-my-projects/wallet-webservice.jpg',
-    altText: 'Wallet Webservice',
-    skills: 'React, JavaScript, Node JS, Git',
-    name: 'Wallet Webservice',
-    githubLink: 'https://github.com/project-repo'
-  },
-  {
-    imgSrc: '/img/img-my-projects/green-harvest-online-store.jpg',
-    altText: 'Green harvest online store',
-    skills: 'React, JavaScript, Node JS, Git',
-    name: 'Green harvest online store',
-    githubLink: 'https://github.com/project-repo'
-  },
-  {
-    imgSrc: '/img/img-my-projects/english-excellence-webservice.jpg',
-    altText: 'English excellence webservice',
-    skills: 'React, JavaScript, Node JS, Git',
-    name: 'English excellence webservice',
-    githubLink: 'https://github.com/project-repo'
-  },
-  {
-    imgSrc: '/img/img-my-projects/power-pulse-webservice.jpg',
-    altText: 'Power pulse webservice',
-    skills: 'React, JavaScript, Node JS, Git',
-    name: 'Power pulse webservice',
-    githubLink: 'https://github.com/project-repo'
-  },
-  {
-    imgSrc: '/img/img-my-projects/mimino-website.jpg',
-    altText: 'Mimino website',
-    skills: 'React, JavaScript, Node JS, Git',
-    name: 'Mimino website',
-    githubLink: 'https://github.com/project-repo'
-  },
-  {
-    imgSrc: '/img/img-my-projects/vyshyvanka-vibes-landing-page.jpg',
-    altText: 'Vyshyvanka vibes Landing Page',
-    skills: 'React, JavaScript, Node JS, Git',
-    name: 'Vyshyvanka vibes Landing Page',
-    githubLink: 'https://github.com/project-repo'
-  },
-  {
-    imgSrc: '/img/img-my-projects/chego-jewelry-website.jpg',
-    altText: 'Chego jewelry website',
-    skills: 'React, JavaScript, Node JS, Git',
-    name: 'Chego jewelry website',
-    githubLink: 'https://github.com/project-repo'
-  },
-  {
-    imgSrc: '/img/img-my-projects/energy-flow-webservice.jpg',
-    altText: 'Energy flow webservice',
-    skills: 'React, JavaScript, Node JS, Git',
-    name: 'Energy flow webservice',
-    githubLink: 'https://github.com/project-repo'
-  },
-  {
-    imgSrc: '/img/img-my-projects/fruitbox-online-store.jpg',
-    altText: 'Fruitbox online store',
-    skills: 'React, JavaScript, Node JS, Git',
-    name: 'Fruitbox online store',
-    githubLink: 'https://github.com/project-repo'
-  },
-  {
-    imgSrc: '/img/img-my-projects/starlight-studio-landing-page.jpg',
-    altText: 'Starlight studio landing page',
-    skills: 'React, JavaScript, Node JS, Git',
-    name: 'Starlight studio landing page',
-    githubLink: 'https://github.com/project-repo'
-  }
-];
-
-// Функція для завантаження проєктів
 function loadProjects() {
   for (let i = 0; i < projectsPerLoad; i++) {
     if (projectsLoaded < totalProjects.length) {
@@ -113,7 +21,7 @@ function loadProjects() {
           <a href="${project.githubLink}" target="_blank" class="button-link">
             Visit
             <svg class="link-icon" width="24" height="24">
-              <use href="/img/sprite.svg#arrow"></use>
+              <use href="./img/icons.svg#arrow"></use>
             </svg>
           </a>
         </div>
@@ -123,9 +31,8 @@ function loadProjects() {
     }
   }
 
-  // Якщо всі проекти завантажені, ховаємо кнопку "Load more"
   if (projectsLoaded >= totalProjects.length) {
-    loadMoreBtn.style.display = 'none';  
+    loadMoreBtn.style.display = 'none';
     iziToast.info({
       title: 'Info',
       message: 'There are no more projects to display.',
@@ -134,16 +41,85 @@ function loadProjects() {
   }
 }
 
+const totalProjects = [
+  {
+    imgSrc: './img/my-projects/wallet-webservice.webp',
+    altText: 'Wallet Webservice',
+    skills: 'React, JavaScript, Node JS, Git',
+    name: 'Wallet Webservice',
+    githubLink: 'https://github.com/project-repo',
+  },
+  {
+    imgSrc: './img/my-projects/green-harvest-online-store.webp',
+    altText: 'Green harvest online store',
+    skills: 'React, JavaScript, Node JS, Git',
+    name: 'Green harvest online store',
+    githubLink: 'https://github.com/project-repo',
+  },
+  {
+    imgSrc: './img/my-projects/english-excellence-webservice.webp',
+    altText: 'English excellence webservice',
+    skills: 'React, JavaScript, Node JS, Git',
+    name: 'English excellence webservice',
+    githubLink: 'https://github.com/project-repo',
+  },
+  {
+    imgSrc: './img/my-projects/power-pulse-webservice.webp',
+    altText: 'Power pulse webservice',
+    skills: 'React, JavaScript, Node JS, Git',
+    name: 'Power pulse webservice',
+    githubLink: 'https://github.com/project-repo',
+  },
+  {
+    imgSrc: './img/my-projects/mimino-website.webp',
+    altText: 'Mimino website',
+    skills: 'React, JavaScript, Node JS, Git',
+    name: 'Mimino website',
+    githubLink: 'https://github.com/project-repo',
+  },
+  {
+    imgSrc: './img/my-projects/vyshyvanka-vibes-landing-page.webp',
+    altText: 'Vyshyvanka vibes Landing Page',
+    skills: 'React, JavaScript, Node JS, Git',
+    name: 'Vyshyvanka vibes Landing Page',
+    githubLink: 'https://github.com/project-repo',
+  },
+  {
+    imgSrc: './img/my-projects/chego-jewelry-website.webp',
+    altText: 'Chego jewelry website',
+    skills: 'React, JavaScript, Node JS, Git',
+    name: 'Chego jewelry website',
+    githubLink: 'https://github.com/project-repo',
+  },
+  {
+    imgSrc: './img/my-projects/energy-flow-webservice.webp',
+    altText: 'Energy flow webservice',
+    skills: 'React, JavaScript, Node JS, Git',
+    name: 'Energy flow webservice',
+    githubLink: 'https://github.com/project-repo',
+  },
+  {
+    imgSrc: './img/my-projects/fruitbox-online-store.webp',
+    altText: 'Fruitbox online store',
+    skills: 'React, JavaScript, Node JS, Git',
+    name: 'Fruitbox online store',
+    githubLink: 'https://github.com/project-repo',
+  },
+  {
+    imgSrc: './img/my-projects/starlight-studio-landing-page.webp',
+    altText: 'Starlight studio landing page',
+    skills: 'React, JavaScript, Node JS, Git',
+    name: 'Starlight studio landing page',
+    githubLink: 'https://github.com/project-repo',
+  },
+];
 
 loadMoreBtn.addEventListener('click', loadProjects);
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  loadProjects();  
+  loadProjects();
 
-  
   if (projectsLoaded < totalProjects.length) {
-    loadMoreBtn.style.display = 'block';  
+    loadMoreBtn.style.display = 'block';
   }
 });
-
